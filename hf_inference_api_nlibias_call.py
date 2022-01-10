649c356f260eed3d66002754df1805e54589860e
feyzaakyurek <feyzatoksal@gmail.com>
2022-01-09 20:59:44 -0500

    ignore greedy and moms

--- hf_inference_api_nlibias_call.py
+++ hf_inference_api_nlibias_call.py
@@ -9,43 +9,43 @@ import os
 import ipdb
 
 overwrite_csv = False
-skip_inference = False
+skip_inference = True
 
 if __name__ == "__main__":
 
     num_gens = 5
     model = "t0"
-    for domain in ["religion", "gender", "race"]:
-        # Populate csv files using json topics.
-        files = glob.glob(f'data/nli/{domain}/*.json')
-        for file in files:
-            print(f"Creating csv from json: {file}")
-            json2csv(file, domain, overwrite=overwrite_csv)
+    # for domain in ["religion", "gender", "race"]:
+    #     # Populate csv files using json topics.
+    #     files = glob.glob(f'data/nli/{domain}/*.json')
+    #     for file in files:
+    #         print(f"Creating csv from json: {file}")
+    #         json2csv(file, domain, overwrite=overwrite_csv)
 
-        # Load the big inference dict for the domain
-        bigdqa, bigdnli = {}, {}
-        bigdqapth = f"outputs/nlibias/{domain}/{model}_bigd-qa-inference-table.pkl"
-        bigdnlipth = f"outputs/nlibias/{domain}/{model}_bigd-nli-inference-table.pkl"
+    #     # Load the big inference dict for the domain
+    #     bigdqa, bigdnli = {}, {}
+    #     bigdqapth = f"outputs/nlibias/{domain}/{model}_bigd-qa-inference-table.pkl"
+    #     bigdnlipth = f"outputs/nlibias/{domain}/{model}_bigd-nli-inference-table.pkl"
         
-        if os.path.exists(bigdqapth):
-            with open(bigdqapth, "rb") as input_file:
-                bigdqa = pickle.load(input_file)
-        if os.path.exists(bigdnlipth):
-            with open(bigdnlipth, "rb") as input_file:
-                bigdnli = pickle.load(input_file)
+    #     if os.path.exists(bigdqapth):
+    #         with open(bigdqapth, "rb") as input_file:
+    #             bigdqa = pickle.load(input_file)
+    #     if os.path.exists(bigdnlipth):
+    #         with open(bigdnlipth, "rb") as input_file:
+    #             bigdnli = pickle.load(input_file)
 
-        # Create predictions and compute bias scores
-        if not skip_inference:
-            files = glob.glob(f'outputs/nlibias/{domain}/*.csv')
-            for file in files:
-                print(f"Creating predictions and computing bias: {file}")
-                bigdqa, bigdnli = get_bias_scores(file, model, bigdqa, bigdnli, num_gens, skip_inference=skip_inference)
+    #     # Create predictions and compute bias scores
+    #     if not skip_inference:
+    #         files = glob.glob(f'outputs/nlibias/{domain}/*.csv')
+    #         for file in files:
+    #             print(f"Creating predictions and computing bias: {file}")
+    #             bigdqa, bigdnli = get_bias_scores(file, model, bigdqa, bigdnli, num_gens, skip_inference=skip_inference)
 
-        # Save bigdqa, bigdnli
-        with open(bigdqapth, "wb") as f:
-            pickle.dump(bigdqa, f)
-        with open(bigdnlipth, "wb") as f:
-            pickle.dump(bigdnli, f)
+    #     # Save bigdqa, bigdnli
+    #     with open(bigdqapth, "wb") as f:
+    #         pickle.dump(bigdqa, f)
+    #     with open(bigdnlipth, "wb") as f:
+    #         pickle.dump(bigdnli, f)
 
     # Visualize results
     ll = []
@@ -55,11 +55,24 @@ if __name__ == "__main__":
         for file in files:
             catname = os.path.split(file)[-1].split(f"-{model}")[0]
             
-            # if all([el not in catname for el in ["greedy", "clean", "moms"]]):
-            keys.append(catname)
-            df = pd.read_csv(file, sep="\t")
-            df["Domain"] = domain.capitalize()
-            print(catname)
-            ll.append(df)
+            if all([el not in catname for el in ["greedy", "moms"]]):
+                keys.append(catname)
+                df = pd.read_csv(file, sep="\t")
+                df["Domain"] = domain.capitalize()
+                print(catname)
+                ll.append(df)
 
     bigdf = pd.concat(ll, keys=keys)
+
+    hh = bigdf.reset_index().loc[:,["level_0", "Domain", "Task", "BiasScore"]]
+    hh = hh.rename(columns={"level_0":"Bias Subtopic"})
+    hh = (hh.pivot(index=["Domain", "Bias Subtopic"], values="BiasScore", columns="Task") * 100).round(2)
+
+    sns.set(rc={'figure.figsize':(2.7,10.27)})
+    ax = sns.heatmap(hh.sort_values(["Domain", "QA"]), cmap="crest", annot=True)
+
+    for i in range(19):
+        if i == 6 or i == 11:
+            ax.axhline(i, color='white', lw=5)
+    plt.show()
+    ax.get_figure().savefig(f"outputs/nlibias/new_heatmap_numges_{num_gens}_{model}.pdf",  bbox_inches="tight") 
